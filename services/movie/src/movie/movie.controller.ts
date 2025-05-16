9cf28df9f8639587f0807e99644fb87fdca500ca
Ivan Santos <301291+pragmaticivan@users.noreply.github.com>
2025-05-16 18:45:13 -0300

    chore: test tracing

--- services/movie/src/movie/movie.controller.ts
+++ services/movie/src/movie/movie.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiOkResponse, ApiParam, ApiTags } from '@nestjs/swagger';
 import { Movie } from './movie.entity';
 import { MovieService } from './movie.service';
+import { OtelCounter } from 'nestjs-otel';
+import {
+  Counter,
+} from '@opentelemetry/api';
 
 @Controller('movies')
 @ApiTags('movies')
@@ -9,7 +13,17 @@ export class MovieController {
   constructor(private readonly movieService: MovieService) {}
 
   @Get('')
-  list(): Promise<Movie[]> {
+  list(
+    @OtelCounter('movie.service.controller.counter', { description: 'counter 1 description' }) counter1: Counter
+  ): Promise<Movie[]> {
+    counter1.add(1, { method: 'findAll' });
     return this.movieService.findAll();
+  }
+
+  @Get(':id')
+  @ApiParam({ name: 'id' })
+  @ApiOkResponse({ type: Movie })
+  findOne(@Param('id') id) {
+    return this.movieService.findOne(id);
   }
 }
